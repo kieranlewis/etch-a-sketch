b4cefede33bacb9f878a1597eac679bfbc56252d
Kieran Lewis <kieranlewis1334@gmail.com>
2021-08-12 01:26:30 +1200

    Added event listener on mouse over to change background of grid item

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function createGrid() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             const div = document.createElement('div');
-            //div.textContent = `Div: [${i},${j}]`;
             div.setAttribute('class', 'grid-item');
+            div.setAttribute('data-grid', `[${i},${j}]`)
+            div.addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'black');
             container.appendChild(div);
         }
         
